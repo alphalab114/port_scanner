12576e0f13a44f490459ee0a8187bdd7eab8be9f
Kali@alphalab <aml2024@gmx.de>
2025-11-10 17:25:24 -0500

    added socket, added first inputs, defined main function

--- portscanner_main.py
+++ portscanner_main.py
@@ -1,6 +1,42 @@
+
 #===========================================================
 #      Port Scanner
 #    only for Educational Purposes
 # writen by Spongebob Squarepants and Tadeus Tentakel
 #
 #============================================================
+
+#===========================================================
+#       IMPORTS
+
+import socket
+
+
+
+#===========================================================
+#      INPUTS
+
+from_port= input("From Port:")
+to_port = input("To Port: ")
+target_ip = input("Target IP : (x.x.x.x) : ")
+
+
+
+print( from_port + to_port + target_ip) #for debug
+
+
+
+
+
+
+
+for port in range(from_port,to_port):
+	scan_socket(target_ip,port)
+
+
+
+
+
+
+
+
