9de97f9aca1440272d37eb2ce7f9d1b3ecc014dd
Kali@alphalab <aml2024@gmx.de>
2025-11-10 17:47:39 -0500

    first working version, port scanner working, no IO at this state

--- portscanner_main.py
+++ portscanner_main.py
@@ -16,22 +16,37 @@ import socket
 #===========================================================
 #      INPUTS
 
-from_port= input("From Port:")
-to_port = input("To Port: ")
-target_ip = input("Target IP : (x.x.x.x) : ")
+#from_port= input("From Port:")
+#to_port = input("To Port: ")
+#target_ip = input("Target IP : (x.x.x.x) : ")
+
+from_port=1
+to_port=100
+
+target_ip = "10.10.251.138"
+
+
+print( from_port, to_port, target_ip) #for debug
+
+
+def scan_socket(host,port):
+	s = socket.socket()
+	s.settimeout(0.5)
+	res = s.connect_ex((host,port)) # needs 1 Argument (address) so we make it a tuple  by (())
+	s.close()
+	return res == 0
 
 
 
-print( from_port + to_port + target_ip) #for debug
+
+for port in range(int(from_port), int(to_port)):
+	if scan_socket(target_ip,port):
+		print(port, "open")
 
 
 
 
 
-
-
-for port in range(from_port,to_port):
-	scan_socket(target_ip,port)
 
 
 
