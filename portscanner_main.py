92dda96e8389cfd5d5feb531cbc37d3c526dd15a
Alphalab <aml2024@gmx.de>
2025-11-11 20:49:24 +0100

    added function scan rage of ports, added function print all ports
    it a working branch

--- portscanner_main.py
+++ portscanner_main.py
@@ -1,7 +1,7 @@
 
 #===========================================================
-#      Port Scanner
-#    only for Educational Purposes
+# Port Scanner
+# only for Educational Purposes
 # writen by Spongebob Squarepants and Tadeus Tentakel
 #
 #============================================================
@@ -38,44 +38,64 @@ print( from_port, to_port, target_ip) #for debug
 #         Classes
 
 
-class Port:
+class CLASS_Ports:
 	def __init__(self, number):
 		self.number = number
 		self.open = False
 
 
-class port_scanner:
+class CLASS_Port_Scanner:
 	def __init__(self, host, timeout):
+		print("init")
 		self.host=host
 		self.timeout = timeout
-		#self.startport = startport
-		#self.endport = endport
+		self.results = []
+		
 
 
 	def _scan_one_port(self,port_number):
-		p = Port(port_number)
+		print("call scan one port")
+		p = CLASS_Ports(port_number)
 		try:
 			with socket.socket() as s:
 				s.settimeout(self.timeout)
 				res = s.connect_ex((self.host,port_number)) # needs 1 Argument (address) so we make it a tuple  by (())
+				print(port_number)
 			if res == 0:
 				p.open=True
 			else:
 				p.open=False
 			s.close()
 		except:
-			print("exception line64")	
+			print("--exception _scan_one_port--")	
 			pass
 		return p
 
 
-"""
-	def _scan_range_of_ports(self,self.startport,self.endport):
-		for port_to_scan in range(self.startport ,self.endport):
-			if _scan_one_port(target_ip,port_to_scan):
-			
-"""	
 
+	def scan_range_of_port(self, startport, endport):
+		print("call scanrange of ports", startport,endport)
+		self.results = []
+		for port_to_scan in range(startport, endport):
+			self.results.append(self._scan_one_port(port_to_scan))
+		return self.results
+		
+
+
+	def print_all_ports(self):
+		print("print findings call")
+		for p in self.results:
+			if p.open == True:
+				print("Port: " , p.number , " open")
+			else:
+				print("Port: " , p.number , " NA")
+
+
+
+
+
+
+			
 
 
 
@@ -85,15 +105,18 @@ class port_scanner:
 
 if __name__ == "__main__":
 
-	host_input = input("Host eigeben:")
-	start_port_input = input("startport eingeben")
-	end_port_input = input("Endport eigeben")
+	host_input = "192.168.178.1"   	#input("Host eigeben:")
+	start_port_input = int(0) 		#input("startport eingeben")
+	end_port_input = int(100)   	#input("Endport eigeben")
 
 
-scanner = port_scanner(host_input, timeout=0.4)
-result = scanner._scan_one_port(80)
+scanner = CLASS_Port_Scanner(host_input, timeout=0.4)
+scanner.scan_range_of_port(start_port_input,end_port_input)
+scanner.print_all_ports()
 
-print(result.number, result.open)
+
+
+
 
 
 
